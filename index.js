0f330ddb84cc80f7aab451b87ac649c87028006f
asolermaria <asolermaria@gmail.com>
2026-03-05 20:06:56 +0100

    100% done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //  1.- Crea un objeto de nombre Coche que tenga las propiedades: marca, modelo, matricula
 const Coche = {
-  marca: "Renault",
-  modelo: "Clio",
-  matricula: "8332KJK",
+  marca: "Seat",
+  modelo: "Ibiza",
+  matricula: "8053FLG",
 };
 
 //  2.- Crea un objeto de nombre Casa que tenga las propiedades: codPostal, calle, portal, piso
@@ -89,23 +89,38 @@ const Avion = {
 };
 
 //  16.- Crea un objeto de nombre Paquete que tenga las propiedades: array contenido con todos los objetos que contenga el paquete
+const Paquete = {contenido: []}
 
 //  17.- Crea un objeto de nombre Pais que tenga las propiedades: numHabitantes, continente, gentilicio
+const Pais = {
+    numHabitantes: "",
+    continente: "",
+    gentilicio: ""
+}
 
 //  18.- Dado un objeto de nombre O_Error obtén el valor de la propiedad codigo guardándolo en la variable codError
+const codError = O_Error.codigo
 
 //  19.- Dado un objeto de nombre Grupo obtén el valor de la propiedad array integrantes guardándolo en la variable integrantes
+const integrantes = Grupo.integrantes
 
 //  20.- Dado un objeto de nombre Impresora obtén el valor de la propiedad objeto tinta{rojo, verde, azul} guardándolo en la variable nivelesTinta
+const nivelesTinta = Impresora.tinta
 
 //  21.- Dado un objeto de nombre Pantalla obtén el valor de la propiedad array bidimensional pixeles guardándolo en la variable pixeles
+const pixeles = Pantalla.pixeles
 
 //  22.- Dado un objeto de nombre Movil obtén el valor de la propiedad especificaciones con ["especificaciones"] guardándolo en la variable especificaciones
+const especificaciones = Movil.especificaciones
 
 //  23.- Dado un objeto de nombre Grupo modifica el valor de la propiedad numIntegrantes por el valor 5
+Grupo.numIntegrantes = 5
 
 //  24.- Dado un objeto de nombre Pantalla modifica el valor de la propiedad dimensiones por el valor 1920x1080
+Pantalla.dimensiones = "1920x1080"
 
 //  25.- Dado un objeto de nombre Led modifica el valor de la propiedad encendido por el valor false si vale true y true si vale false
+Led.encendido = !Led.encendido
 
 //  26.- Dado un objeto de nombre Movil modifica el valor de la propiedad temperatura por el valor 20º
+Movil.temperatura = "20º"
